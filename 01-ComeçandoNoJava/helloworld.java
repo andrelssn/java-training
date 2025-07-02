5247a1445822eb6b71e28b4ab12e128dc1884752
André Santos <andreluissilsan@hotmail.com>
2025-07-02 11:46:00 -0300

    module 01 completed

---
+++ 01-ComeçandoNoJava/helloworld.java
@@ -0,0 +1,9 @@
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+// RODAR NO TERMINAL :
+    // compilar com javac (nome do arquivo java)
+    // executar class com java (nome do arquivo class)
