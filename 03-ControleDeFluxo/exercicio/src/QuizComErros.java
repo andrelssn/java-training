8c5f9b664797608642fd631dd1e4d7191d454364
André Santos <andreluissilsan@hotmail.com>
2025-07-04 12:47:00 -0300

    module04

---
+++ 03-ControleDeFluxo/exercicio/src/QuizComErros.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class QuizComErros {
+    public static void main(String[] args) {
+        String a = "1) Max Verstappen";
+        String b = "2) Lewis Hamilton";
+        String c = "3) Charles Leclerc";
+        String space = " ";
+        int resCorrect = 2;
+        int res;
+
+
+        System.out.println("Quem tem 7 titulos?");
+        System.out.println(a + space + b + space + c);
+
+        Scanner scanner = new Scanner(System.in);
+        res = scanner.nextInt();
+
+        if (res == resCorrect) {
+            System.out.println("Correto!");
+        } else {
+            System.out.println("Errado.");
+        }
+    }
+}
